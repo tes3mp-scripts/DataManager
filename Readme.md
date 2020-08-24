f12dd59b27f0ca1a6c9ca78dba22db3a305d499e
uramer <antonuramer@gmail.com>
2020-08-24 21:47:01 +0200

    Intercompatibility between 0.7 and 0.8

--- Readme.md
+++ Readme.md
@@ -1,9 +1,9 @@
-This module does nothing by itself, but provides a simple API to handle configuration and data files. Is required for all of my moudles.
+This script does nothing by itself, but provides a simple API to handle configuration and data files. It is required for all of my modules.
 
-Has to be `require`d before any of the modules that use it.
+Installation instructions: https://github.com/tes3mp-scripts/Tutorials
 
-Using it in your modules
----
+# Using it in your scripts
+
 The following is a list of all functions in this module (and their arguments) you are expected to use in your scripts. If you required it in standard way, they will be accessible from the global `DataManager` table.
 * `loadConfiguration`
   * `scriptName` name by which you will be requesting your configuration file.
@@ -17,3 +17,32 @@ The following is a list of all functions in this module (and their arguments) yo
   * `scriptName`
   * `data` data to write to the file
   * `keyOrder` array which defines order in which json keys should be written into the file jsonInterface.save method
+
+# 0.8 future-proofing
+
+[My branch of CoreScripts](https://github.com/uramer/CoreScripts/tree/0.7.1-async) (likely to be merged for 0.8 release) provides configuration and data storage APIs out of the box.  
+If you want your script to function both in 0.7 and 0.8 (possible for most, maybe even all 0.7 scripts), you have two options:
+1. Use the new API (recommended)
+    * Your script will require `DataManager` for 0.7, but not for 0.8
+    * When running 0.7, data will only be saved on server exit, and not periodically
+2. Keep using `DataManager`, which will use the new implementation automatically  
+    Your script will require `DataManager` both for 0.7 and 0.8
+    There are a couple of nuances when running on 0.8:
+    * `saveData` does nothing, data will be saved automatically instead
+    * just like when using `storage`, if you want it data to be saved at all,
+    you must alter the table you received from `loadData`, rather than create a new one when you change it  
+    Example:
+    ```Lua
+      MyScript.data = DataManager.loadData("MyScript", {})
+      MyScript.data.players = {} -- ok, will be saved correctly
+      MyScript.data = { players = {} } -- not ok, will not be saved
+      MyScript.data = tableHelper.deepCopy(MyScript.data) -- not ok either
+    ```
+    * for servers running Postgres, you must only `loadData` on or after `OnServerInit` event, e.g.:
+      ```Lua
+        customEventHooks.registerHandler('OnServerInit', function(eventStatus)
+          if not eventStatus.validDefaultHandler then return end
+          MyScript.data = DataManager.loadData("MyScript", {})
+        end)
+      ```
+    * `keyOrder` argument of `loadData` will be ignored for performance reasons
--- main.lua
+++ main.lua
@@ -3,61 +3,99 @@ DataManager = {}
 DataManager.configPrefix = "custom/__config_"
 DataManager.dataPrefix = "custom/__data_"
 
-function DataManager.getConfigPath(scriptName)
+local function getConfigPath(scriptName)
     return DataManager.configPrefix .. scriptName .. ".json"
 end
 
-function DataManager.saveConfiguration(scriptName, config, keyOrder)
-    local filePath = DataManager.getConfigPath(scriptName)
-    return jsonInterface.save(filePath, config, keyOrder)
-end
-
-function DataManager.checkForNils(t, d)
-    for k, v in pairs(d) do
-        if t[k] == nil then
-            t[k] = v
-        else
-            if type(d[k]) == "table" then
-                if type(t[k]) == "table" then
-                    DataManager.checkForNils(t[k], d[k])
-                else
-                    t[k] = d[k]
-                end
-            end
-        end
-    end
-    return t
-end
-
-function DataManager.loadConfiguration(scriptName, defaultConfig, keyOrder)
-    local filePath = DataManager.getConfigPath(scriptName)
-    local config = jsonInterface.load(filePath)
-    if config == nil then
-        config = defaultConfig
-    end
-    DataManager.saveConfiguration(scriptName, config, keyOrder)
-    return config
-end
-
-
-
-function DataManager.getDataPath(scriptName)
+local function getDataPath(scriptName)
     return DataManager.dataPrefix .. scriptName .. ".json"
 end
 
-function DataManager.saveData(scriptName, data, keyOrder)
-    local filePath = DataManager.getDataPath(scriptName)
-    return jsonInterface.save(filePath, data, keyOrder)
+if jsonConfig then
+    function DataManager.saveConfiguration(scriptName, config, keyOrder)
+    end
+
+    function DataManager.loadConfiguration(scriptName, defaultConfig, keyOrder)
+        return jsonConfig.Load(scriptName, defaultConfig, keyOrder)
+    end
+else
+    function DataManager.saveConfiguration(scriptName, config, keyOrder)
+        local filePath = getConfigPath(scriptName)
+        return jsonInterface.save(filePath, config, keyOrder)
+    end
+
+    function DataManager.loadConfiguration(scriptName, defaultConfig, keyOrder)
+        local filePath = getConfigPath(scriptName)
+        local config = jsonInterface.load(filePath)
+        if config == nil then
+            config = defaultConfig
+        end
+        DataManager.saveConfiguration(scriptName, config, keyOrder)
+        return config
+    end
+
+    jsonConfig = {}
+    function jsonConfig.Load(name, default, keyOrderArray)
+        return DataManager.loadConfiguration(name, default, keyOrderArray)
+    end
 end
 
-function DataManager.loadData(scriptName, defaultData, keyOrder)
-    local filePath = DataManager.getDataPath(scriptName)
-    local data = jsonInterface.load(filePath)
-    if data == nil then
-        data = defaultData
+if storage then
+    -- storage data gets saved automatically
+    function DataManager.saveData(scriptName, data, keyOrder)end
+
+    function DataManager.loadData(scriptName, defaultData, keyOrder)
+        storage.Load(scriptName, defaultData)
     end
-    DataManager.saveData(scriptName, data, keyOrder)
-    return data
+else
+    function DataManager.saveData(scriptName, data, keyOrder)
+        local filePath = getDataPath(scriptName)
+        return jsonInterface.save(filePath, data, keyOrder)
+    end
+
+    function DataManager.loadData(scriptName, defaultData, keyOrder)
+        local filePath = getDataPath(scriptName)
+        local data = jsonInterface.load(filePath)
+        if data == nil then
+            data = defaultData
+        end
+        DataManager.saveData(scriptName, data, keyOrder)
+        return data
+    end
+
+    storage = {
+        data = {}
+    }
+
+    function storage.Save(key)
+        DataManager.saveData(key, storage.data[key])
+    end
+
+    function storage.SaveAll()
+        for key in pairs(storage.data) do
+            storage.Save(key)
+        end
+    end
+
+    function storage.SaveAllAsync()
+        return storage.SaveAll() -- no async in 0.7
+    end
+
+    function storage.Load(key, default)
+        if not storage.data[key] then
+            local eventStatus = customEventHooks.triggerValidators('OnStorageLoad', {key})
+            if eventStatus.validDefaultHandler then
+                storage.data[key] = DataManager.loadData(key, default)
+            end
+            customEventHooks.triggerHandlers('OnStorageLoad', eventStatus, {key})
+        end
+        return storage.data[key]
+    end
+
+    customEventHooks.registerHandler("OnServerExit", function(eventStatus)
+        if not eventStatus.validDefaultHandler then return end
+        storage.SaveAll()
+    end)
 end
 
 return DataManager
